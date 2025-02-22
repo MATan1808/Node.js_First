25dbb0c84dde9b9ef4f9cf347742b8f0af277667
Your Name <your.email@example.com>
2025-02-22 15:25:14 +0700

    install morgan

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express'); // Import thư viện Express
+const morgan = require('morgan'); // Import thư viên morgan
 const app = express(); // Tạo một ứng dụng Express
 const port = 3000; // Chọn một cổng để lắng nghe
 
+// Thiết lập body-parser để chuyển dữ liệu từ request POST thành JSON
+// Sử dụng Morgan để log request
+app.use(morgan('dev'));
 app.get('/trangtru', (req, res) => { // Định nghĩa một route cho đường dẫn gốc '/'
     res.send('Hello World!'); // Gửi phản hồi "Hello World!"
 });
